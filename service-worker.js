fa30b2b32a35488e39664585d209899a3789bf92
yusrinulkarimah <yusrinulkarimah@gmail.com>
2026-08-30 14:01:59 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-const C='stok-rumah-v1',A=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C='stok-rumah-v1.1',A=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
