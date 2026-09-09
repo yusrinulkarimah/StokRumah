645a65598b7524e0cad7001873d389286f9d55ec
yusrinulkarimah <yusrinulkarimah@gmail.com>
2026-09-09 20:46:25 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-const CACHE='stok-rumah-v2.4.1';
-const CORE=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));});
-self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim();})()));
+const CACHE='stok-rumah-v2.5-stable';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim();})()));
 self.addEventListener('fetch',e=>{
  if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
- }else{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+   e.respondWith(fetch(e.request,{cache:'reload'}).catch(()=>caches.match('./index.html')));
  }
 });
